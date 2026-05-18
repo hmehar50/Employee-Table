257b1d7cb263aa9f5b6ba3cf7d25b830e1166543
Harsh Mehar <hmehar+cisco@cisco.com>
2026-05-18 09:32:53 +0530

    backend

---
+++ backend/src/controllers/employeeController.js
@@ -0,0 +1,12 @@
+import Employee from "../models/Employee";
+import logger from "../config/logger";
+
+export const getEmployee = async (req, res) => {
+    try{
+        const {
+            page = 1,
+            sortBy = 'employeeId',
+            order = 'asc',
+        }
+    }
+}
